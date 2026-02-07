fdfb1f8fc6e9dcdff7ee2fbde5ffff500d2189e8
temeaco-max <temea.co@gmail.com>
2026-02-07 15:52:29 +0000

    Create Sequelize model for Orders

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,1 +1,53 @@
-const { Model, DataTypes } = require('sequelize');\n\nclass Order extends Model {}\n\nOrder.init({\n  id: {\n    type: DataTypes.INTEGER,\n    autoIncrement: true,\n    primaryKey: true,\n  },\n  buyer_id: {\n    type: DataTypes.INTEGER,\n    allowNull: false,\n  },\n  seller_id: {\n    type: DataTypes.INTEGER,\n    allowNull: false,\n  },\n  status: {\n    type: DataTypes.ENUM('pending', 'paid', 'shipped', 'delivered'),\n    defaultValue: 'pending',\n  },\n  total_price: {\n    type: DataTypes.DECIMAL(10, 2),\n    allowNull: false,\n  },\n  items: {\n    type: DataTypes.JSON,\n    allowNull: false,\n  },\n  created_at: {\n    type: DataTypes.DATE,\n    defaultValue: new Date(),\n  },\n  shipped_at: {\n    type: DataTypes.DATE,\n    allowNull: true,\n  },\n  delivered_at: {\n    type: DataTypes.DATE,\n    allowNull: true,\n  },\n}, {\n  sequelize,\n  modelName: 'Order',\n  timestamps: false,\n});\n\nmodule.exports = Order;\n
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Order extends Model {}
+
+  Order.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    buyer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    seller_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('pending', 'paid', 'shipped', 'delivered'),
+      allowNull: false,
+    },
+    total_price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    items: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    shipped_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    delivered_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }, {
+    sequelize,
+    modelName: 'Order',
+    tableName: 'orders',
+    timestamps: false,
+  });
+
+  return Order;
+};
