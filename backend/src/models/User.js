38299ec25eb7032f0248ae2fdac52ee03ee8f9f9
temeaco-max <temea.co@gmail.com>
2026-02-07 15:52:48 +0000

    Add User model with Sequelize

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,60 +1,62 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 
-class User extends Model {
-  static associate(models) {
-    // define association here
-  }
-
-  // Password hashing before saving the user
-  static async hashPassword(user) {
-    if (user.changed('password')) {
-      user.password = await bcrypt.hash(user.password, 10);
+module.exports = (sequelize) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
     }
   }
-}
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('buyer', 'seller'),
+      allowNull: false,
+    },
+    avatar_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      onUpdate: DataTypes.NOW,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    underscored: true,
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  });
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM('buyer', 'seller'),
-    allowNull: false,
-  },
-  avatar_url: {
-    type: DataTypes.STRING,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-}, {
-  sequelize,
-  modelName: 'User',
-  hooks: {
-    beforeSave: User.hashPassword,
-  },
-  underscored: true,
-});
+  User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  });
 
-module.exports = User;
+  return User;
+};
